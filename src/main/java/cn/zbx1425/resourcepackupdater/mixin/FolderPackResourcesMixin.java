03955ff4add319281127877a1ddb98955ebd590a
qha821 <qingha821@outlook.com>
2025-02-07 10:19:52 +0800

    Maybe Works

--- src/main/java/cn/zbx1425/resourcepackupdater/mixin/FolderPackResourcesMixin.java
+++ src/main/java/cn/zbx1425/resourcepackupdater/mixin/FolderPackResourcesMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.AbstractPackResources;
 import net.minecraft.server.packs.PathPackResources ;
 import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.ResourcePackFileNotFoundException;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
