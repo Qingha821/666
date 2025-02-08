d38ce90f9ec462acaaecd85b2cb6e8aa8c0b8e22
Qing Huian <qingha821@outlook.com>
2025-02-08 10:48:25 +0800

    Update OptionsScreenMixin.java

--- src/main/java/cn/zbx1425/resourcepackupdater/mixin/OptionsScreenMixin.java
+++ src/main/java/cn/zbx1425/resourcepackupdater/mixin/OptionsScreenMixin.java
@@ -1,6 +1,8 @@
 package cn.zbx1425.resourcepackupdater.mixin;
 
 import cn.zbx1425.resourcepackupdater.ResourcePackUpdater;
+import cn.zbx1425.resourcepackupdater.ResourcePackUpdaterClient;
+import net.minecraft.client.Options;
 import net.minecraft.client.gui.screens.OptionsScreen;
 import net.minecraft.server.packs.repository.PackRepository;
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,17 +10,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(OptionsScreen.class)
-public class OptionsScreenMixin {
+@Mixin(Options.class)
+public class OptionsMixin {
 
-    @Inject(
-        method = "updatePackList(Lnet/minecraft/server/packs/repository/PackRepository;)V",
-        at = @At(
-            value = "INVOKE", 
-            target = "Lnet/minecraft/client/Options;save()V"
-        )
-    )
-    private void updatePackList(PackRepository packRepository, CallbackInfo ci) {
-        ResourcePackUpdater.modifyPackList();
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Options;save()V"), method = "updateResourcePacks")
+    void updatePackList(PackRepository packRepository, CallbackInfo ci) {
+        ResourcePackUpdaterClient.modifyPackList();
     }
 }
