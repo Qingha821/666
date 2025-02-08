9550ca5e173f1ccf43a5762141b840b6eb0e612e
Qing Huian <qingha821@outlook.com>
2025-02-08 12:26:25 +0800

    Update OptionsScreenMixin.java

--- src/main/java/cn/zbx1425/resourcepackupdater/mixin/OptionsScreenMixin.java
+++ src/main/java/cn/zbx1425/resourcepackupdater/mixin/OptionsScreenMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(OptionsScreen.class)
+@Mixin(Options.class)
 public class OptionsScreenMixin {
 
     @Inject(
